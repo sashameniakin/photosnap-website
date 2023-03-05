02a08b9b06d8dadbf4ac2d43c49ff7cf541d73d3
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-03-05 23:00:35 +0100

    pricing page done

--- src/components/Pricing/Item.jsx
+++ src/components/Pricing/Item.jsx
@@ -1,3 +1,16 @@
-export default function Item({ children }) {
-  return <p className="py-6">{children}</p>;
+export default function Item({ check, blackLinie, title, description }) {
+  return (
+    <p
+      className={`py-6 h-[63px] ${!description ? "flex justify-center" : ""} ${
+        title ? "pl-5" : ""
+      } ${
+        blackLinie
+          ? "border-b-pureBlack border-b-[1px]"
+          : "border-b-lightGrey border-b-[1px]"
+      }`}
+    >
+      {check && <img src="../images/pricing/check.svg" alt="check" />}
+      {description}
+    </p>
+  );
 }
